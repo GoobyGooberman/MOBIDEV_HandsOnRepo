c52c48c8fb5adc6ecdedd7682ea68013a827263f
STC-LAB\NeilDG <neil.delgallego@dlsu.edu.ph>
2018-02-08 14:18:39 +0800

    Created template for music player. TODO:// Connect music player activity to nav.

--- app/src/main/java/com/neildg/mobidev_handsonrepo/MainActivity.java
+++ app/src/main/java/com/neildg/mobidev_handsonrepo/MainActivity.java
@@ -16,6 +16,7 @@ import android.view.MenuItem;
 
 import com.neildg.mobidev_handsonrepo.activity_lifecycle.LifecycleActivity;
 import com.neildg.mobidev_handsonrepo.activity_lifecycle.LifecycleActivityWorkspace;
+import com.neildg.mobidev_handsonrepo.activity_music_player.MusicPlayerActivity;
 import com.neildg.mobidev_handsonrepo.activity_restaurant.ChooseRestoWorkActivity;
 
 public class MainActivity extends AppCompatActivity
@@ -99,15 +100,11 @@ public class MainActivity extends AppCompatActivity
             Intent i = new Intent(MainActivity.this, ChooseRestoWorkActivity.class);
             this.startActivity(i);
         }
-        /*if(id == R.id.nav_restaurant) {
-            Intent i = new Intent(MainActivity.this,RestaurantActivity.class);
+
+        if(id == R.id.nav_music_player) {
+            Intent i = new Intent(MainActivity.this,MusicPlayerActivity.class);
             this.startActivity(i);
         }
-
-        if(id == R.id.nav_restaurant_2) {
-            Intent i = new Intent(MainActivity.this,S14_RestaurantActivity.class);
-            this.startActivity(i);
-        }*/
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
