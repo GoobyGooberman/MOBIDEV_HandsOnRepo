a9399f611e536e3cd1eff023739fcdb72cc09377
NeilDG <neil.delgallego@dlsu.edu.ph>
2018-02-21 12:15:32 +0800

    Added S14 restaurant workspace.

--- app/src/main/java/com/neildg/mobidev_handsonrepo/activity_restaurant_workspace/S14_RestaurantActivity.java
+++ app/src/main/java/com/neildg/mobidev_handsonrepo/activity_restaurant_workspace/S14_RestaurantActivity.java
@@ -1,5 +1,6 @@
 package com.neildg.mobidev_handsonrepo.activity_restaurant_workspace;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -13,8 +14,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.neildg.mobidev_handsonrepo.R;
+import com.neildg.mobidev_handsonrepo.activity_restaurant.AddRestaurantActivity;
+import com.neildg.mobidev_handsonrepo.activity_restaurant.RestaurantModel;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class S14_RestaurantActivity extends AppCompatActivity {
     private final static String TAG = "S14_RestaurantActivity";
@@ -34,7 +39,6 @@ public class S14_RestaurantActivity extends AppCompatActivity {
     }
 
     private void createDefaultRestaurants() {
-
             restoList.add(new S14_RestaurantModel("McDonalds", "Love Ko 'To", 20));
             restoList.add(new S14_RestaurantModel("Jollibee", "Bida ang Saya", 10));
             restoList.add(new S14_RestaurantModel("Zarks", "Greaasy Burgers", 5));
@@ -43,7 +47,7 @@ public class S14_RestaurantActivity extends AppCompatActivity {
             restoList.add(new S14_RestaurantModel("Shakey's", "Pizza", 8));
             restoList.add(new S14_RestaurantModel("Starbucks", "Coffee Place", 10));
             restoList.add(new S14_RestaurantModel("Burger Machine", "Burgers", 6));
-            restoList.add(new S14_RestaurantModel("Subway", "Hotdogs", 14));
+            restoList.add(new S14_RestaurantModel("Subway", "Hotdogs", 50));
             restoList.add(new S14_RestaurantModel("Coreon Gate", "Korean Internet and Food", 3));
 
         //simply log the restaurants in logcat
@@ -69,10 +73,35 @@ public class S14_RestaurantActivity extends AppCompatActivity {
         surpriseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                S14_RestaurantActivity.this.showSnackbarMessage("TEST RESTAURANT");
-                //TODO: replace TEST RESTAURANT with actual restaurant name from list.
+                S14_RestaurantActivity.this.showSnackbarMessage(
+                        randomizeRestaurant().getRestoName());
             }
         });
+
+        Button addBtn = this.findViewById(R.id.s14_add_btn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(S14_RestaurantActivity.this, S14_AddRestaurantActivity.class);
+                startActivityForResult(i, S14_AddRestaurantActivity.ADD_REQUEST_CODE);
+            }
+        });
+    }
+
+    private S14_RestaurantModel randomizeRestaurant() {
+        ArrayList<S14_RestaurantModel> weightedList = new ArrayList<>();
+
+        for(int i = 0; i < this.restoList.size(); i++) {
+            S14_RestaurantModel restaurant = this.restoList.get(i);
+            for(int counter = 0; counter < restaurant.getWeight(); counter++) {
+                weightedList.add(restaurant);
+            }
+        }
+
+        Random random = new Random();
+        int randomNum = random.nextInt(weightedList.size());
+
+        return weightedList.get(randomNum);
     }
 
     private void showSnackbarMessage(String restaurantName) {
@@ -84,5 +113,26 @@ public class S14_RestaurantActivity extends AppCompatActivity {
         snackbarActionTextView.setTypeface(snackbarActionTextView.getTypeface(), Typeface.BOLD);
 
         snackbar.show();
+    }
+
+    private void addRestaurant(String name, String description, int weight) {
+        S14_RestaurantModel restaurantModel = new S14_RestaurantModel(name, description, weight);
+        this.restoList.add(restaurantModel);
+
+        this.restaurantAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == S14_AddRestaurantActivity.ADD_REQUEST_CODE && data != null) {
+            //do something on data
+            String name = data.getStringExtra(S14_AddRestaurantActivity.NAME_KEY);
+            String desc = data.getStringExtra(S14_AddRestaurantActivity.DESC_KEY);
+            int weight = data.getIntExtra(S14_AddRestaurantActivity.WEIGHT_KEY, 0);
+            this.addRestaurant(name, desc, weight);
+            Log.d(TAG, "ON ACTIVITY RESULT! Request code is:" +requestCode);
+        }
     }
 }
