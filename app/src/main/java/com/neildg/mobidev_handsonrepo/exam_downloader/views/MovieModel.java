2533bc2b7c5567cb6d235cc818c641fd21a61bc3
NeilDG <neil.delgallego@dlsu.edu.ph>
2018-03-05 14:07:03 +0800

    Checkpoint for models

---
+++ app/src/main/java/com/neildg/mobidev_handsonrepo/exam_downloader/views/MovieModel.java
@@ -0,0 +1,25 @@
+package com.neildg.mobidev_handsonrepo.exam_downloader.views;
+
+/**
+ * Represents the movie model
+ * Created by delgallegon on 05/03/2018.
+ */
+
+public class MovieModel {
+    private final static String TAG = "MovieModel";
+
+    private String name, description;
+
+    public MovieModel(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+}
---
+++ app/src/main/java/com/neildg/mobidev_handsonrepo/exam_downloader/views/MovieViewHolder.java
@@ -0,0 +1,16 @@
+package com.neildg.mobidev_handsonrepo.exam_downloader.views;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by delgallegon on 05/03/2018.
+ */
+
+public class MovieViewHolder extends RecyclerView.ViewHolder {
+    private final static String TAG = "MovieViewHolder";
+
+    public MovieViewHolder(View view) {
+        super(view);
+    }
+}
